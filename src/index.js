0b152d1f848f992cc4aeea36568177d96b9756c1
godtyze <omgfail301@gmail.com>
2021-12-26 03:36:04 +0600

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let nToString = String(n);
+    let res = '';
+
+    for (let i = 0; i < nToString.length; i++) {
+        res = `${nToString[i]}${res}`;
+    }
+    return parseInt(res);
 }
