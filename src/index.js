15f3c8c37f1cebbf3217b55d8d177162ef0e5003
godtyze <omgfail301@gmail.com>
2022-06-11 20:36:28 +0600

    refactor: refactor implementation of function reverse

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 module.exports = function reverse(n) {
-    let nToString = String(n);
-    let res = '';
-
-    for (let i = 0; i < nToString.length; i++) {
-        res = `${nToString[i]}${res}`;
-    }
-    return parseInt(res);
+    return parseInt(n.toString().split('').reverse().join(''));
 }
